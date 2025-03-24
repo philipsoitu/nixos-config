5c44f76357d3e00f78acf5392402aa5acb2fa496
philipsoitu <philip.soitu@gmail.com>
2025-03-24 16:18:36 -0400

    changes

--- flake.nix
+++ flake.nix
@@ -1,34 +1,16 @@
 {
-  description = "My NixOS configuration with Home Manager";
-
+  description = "A simple NixOS flake";
+  
   inputs = {
-    nixos.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
-  };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  }; 
 
-  outputs = { self, nixos, home-manager, ... }: {
+  outputs = { self, nixpkgs, ... }: {
     nixosConfigurations = {
-
-      nixos-desktop = nixos.lib.nixosSystem {
+      nixos-desktop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [
-          ./nixos-config/configuration.nix
-
-          # Home Manager configuration module
-          home-manager.nixosModules.home-manager
-        ];
-        specialArgs = { inherit home-manager; };
-      };
-
-    };
-
-    # Home Manager configuration (this part will be linked to your user)
-    homeConfigurations = {
-      phil = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixos.pkgs;
-        modules = [ ./home-manager/default.nix ];
-      };
+        modules = [ ./configuration.nix ];
+     };
     };
   };
 }
-
