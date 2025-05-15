49c035d38560b8249c6c0e9b279d5edbc88bf326
phil <philip.soitu@gmail.com>
2025-05-15 10:08:50 -0400

    powersave on battery

--- modules/framework.nix
+++ modules/framework.nix
@@ -3,4 +3,12 @@
 {
   networking.hostName = "framework";
   services.fwupd.enable = true;
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    };
+  };
 }
