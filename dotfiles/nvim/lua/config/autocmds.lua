fd86942fe06901c7ff0438c8af7c431660c15e79
phil <philip.soitu@gmail.com>
2025-05-15 13:15:36 -0400

    autocompile latex nvim

--- dotfiles/nvim/lua/config/autocmds.lua
+++ dotfiles/nvim/lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Define an autocmd group for LaTeX compilation
+vim.api.nvim_create_augroup("AutoCompileLatex", { clear = true })
+
+-- Add the autocmd for LaTeX files
+vim.api.nvim_create_autocmd("BufWritePost", {
+    group = "AutoCompileLatex",
+    pattern = "*.tex",
+    callback = function()
+        local filepath = vim.fn.expand("%:p:h")
+        local filename = vim.fn.expand("%")
+        local cmd = string.format("cd %s && pdflatex -interaction=nonstopmode -halt-on-error %s > pdflatex.log 2>&1", filepath, filename)
+        vim.fn.system(cmd)
+    end,
+    desc = "Auto compile LaTeX files on save",
+})
