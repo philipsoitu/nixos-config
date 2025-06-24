e40f7f942a67761e18ad2ae3809b35669c8999db
Philip Soitu <philip.soitu@gmail.com>
2025-06-24 18:35:38 -0400

    thinkpad config done?

--- modules/common.nix
+++ modules/common.nix
@@ -5,13 +5,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-
+  # Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -27,6 +22,9 @@
     layout = "us";
     variant = "";
   };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.phil = {
--- modules/thinkpad.nix
+++ modules/thinkpad.nix
@@ -1,13 +1,37 @@
 { config, pkgs, inputs, ... }:
 
 {
-  networking.hostName = "thinkpad";
 
-  services.tlp = {
+  networking.hostName = "thinkpad"; 
+
+
+   # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-    };
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
   };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    libreoffice-qt6-fresh
+    hunspell
+    hunspellDicts.en_CA
+  ];
 }
