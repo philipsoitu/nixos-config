f90df4645b39b03e86a4c8ac87378f3def35af2c
phil <philip.soitu@gmail.com>
2025-07-20 16:53:02 -0400

    i3 config starting soon

---
+++ modules/window-managers/i3.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, callPackage, ... }: 
+
+{
+  environment.pathsToLink = [ "/libexec" ]; 
+
+  services.xserver = {
+    enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+   
+    displayManager = {
+        defaultSession = "none+i3";
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        i3status
+     ];
+    };
+  };
+  programs.i3lock.enable = true; #default i3 screen locker
+}
