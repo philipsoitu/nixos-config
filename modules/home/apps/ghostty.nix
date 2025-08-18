bda68d40741072fb39a418b4a7cc27ef9600d69e
philipsoitu <philip.soitu@gmail.com>
2025-08-18 18:47:31 -0400

    ghostty font

--- modules/home/apps/ghostty.nix
+++ modules/home/apps/ghostty.nix
@@ -11,6 +11,10 @@
         theme = "GruvboxDark";
         background-opacity = 0.7;
 
+        font-size = 13;
+        window-inherit-font-size = true;
+
+
         window-padding-balance = true;
         window-padding-x = 0;
         window-padding-y = 0;
