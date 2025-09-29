829ff40a5f5794d852ae93d06b57433e7ffcfc8c
philipsoitu <philip.soitu@gmail.com>
2025-09-29 15:28:16 -0400

    whoops

--- modules/nixos/printer.nix
+++ modules/nixos/printer.nix
@@ -4,9 +4,7 @@
   options.printer.enable = lib.mkEnableOption "Enable printer";
 
   config = lib.mkIf config.printer.enable {
-    environment.systemPackages = [
-      services.printing.enable = true;
-      services.printing.drivers = [ pkgs.brlaser ];
-    ];
+    services.printing.enable = true;
+    services.printing.drivers = [ pkgs.brlaser ];
   };
 }
