bee527713264546a80b33172e6811de03f6d9af6
philipsoitu <philip.soitu@gmail.com>
2025-11-12 14:59:53 -0500

    lua_ls config to find vim

--- modules/home/dev/neovim/lua/lspconfig.lua
+++ modules/home/dev/neovim/lua/lspconfig.lua
@@ -3,4 +3,13 @@ vim.lsp.enable({
   "nil_ls",
   "tinymist",
 })
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      }
+    }
+  }
+})
 
