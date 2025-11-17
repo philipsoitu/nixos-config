69213c4c792f004f9ebccf0ac4fe438e544f7c49
philipsoitu <philip.soitu@gmail.com>
2025-11-17 18:00:49 -0500

    netrw commands

--- modules/home/dev/neovim/lua/netrw.lua
+++ modules/home/dev/neovim/lua/netrw.lua
@@ -3,3 +3,19 @@ vim.g.netrw_banner = 0
 
 -- Netrw keybinds
 vim.keymap.set('n', '<leader>e', ':Explore<CR>')
+
+-- Netrw remaps
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "netrw",
+  callback = function()
+    local buf = 0
+
+    -- Navigation
+    vim.keymap.set('n', 'l', '<CR>', { buffer = buf, desc = "Open file / enter folder" })
+    vim.keymap.set('n', 'h', '-', { buffer = buf, desc = "Go up directory" })
+
+    -- Create
+    vim.keymap.set('n', 'a', '%', { buffer = buf, desc = "Create new file" })
+    vim.keymap.set('n', 'A', 'd', { buffer = buf, desc = "Create new folder" }) -- works reliably
+  end,
+})
