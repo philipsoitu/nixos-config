a1dad8b210ce3149af39b784ee9a737524868bf7
philipsoitu <philip.soitu@gmail.com>
2025-11-23 14:55:12 -0500

    just crtl+l dummy

--- modules/home/dev/neovim/lua/keymaps.lua
+++ modules/home/dev/neovim/lua/keymaps.lua
@@ -1,5 +1,2 @@
 -- Ctrl + S: Save
 vim.keymap.set({ 'n', 'i' }, '<C-s>', '<Esc>:w<CR>', { noremap = true, silent = true })
-
--- Removes highlighting from search
-vim.keymap.set('n', '<Esc><Esc>', ':nohlsearch<CR>')
