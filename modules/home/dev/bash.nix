a9d40e89f8b7ed715853aab386f88beee638fb47
philipsoitu <philip.soitu@gmail.com>
2025-12-11 15:53:37 -0500

    keybinds in bash and ghostty

--- modules/home/dev/bash.nix
+++ modules/home/dev/bash.nix
@@ -6,10 +6,14 @@
   config = lib.mkIf config.bash.enable {
     programs.bash = {
       enable = true;
+
       initExtra = ''
+        # Prompt
         PS1="\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
+
+        # Ctrl+F tmux-sessionizer
+        bind -x '"\C-f":tmux-sessionizer'
       '';
     };
   };
 }
-
