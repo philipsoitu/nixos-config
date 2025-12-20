6316e0a0711d23f9b7b5865762e16b7c8872de78
philipsoitu <philip.soitu@gmail.com>
2025-12-19 23:20:28 -0500

    fixed goto definition to work with lsp

--- modules/home/dev/neovim/lua/lspconfig.lua
+++ modules/home/dev/neovim/lua/lspconfig.lua
@@ -21,3 +21,13 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.lsp.buf.format({ async = false })
   end,
 })
+
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local opts = { buffer = args.buf }
+
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  end,
+})
