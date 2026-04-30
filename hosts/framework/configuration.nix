757220e9d5a163d685471d81d49bf72a43bbab7f
philipsoitu <philip.soitu@gmail.com>
2026-04-30 12:30:40 -0400

    removed ollama

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -22,24 +22,6 @@
   boot.initrd.kernelModules = [ "amdgpu" ];
   hardware.graphics.enable = true;
 
-  services.ollama = {
-    enable = true;
-
-    package = pkgs.ollama-vulkan;
-
-    loadModels = [
-      "qwen2.5:3b"
-      "nomic-embed-text"
-      "qwen3-embedding:0.6b"
-      "qwen3:4b"
-      "qwen3:8b"
-    ];
-
-    environmentVariables = {
-      OLLAMA_VULKAN = "1";
-    };
-  };
-
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
