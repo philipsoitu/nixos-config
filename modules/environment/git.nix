9ae9704e77a8ebdf8130c6a60a675130852a752d
philipsoitu <philip.soitu@gmail.com>
2026-05-02 18:30:14 -0400

    fixed git

--- modules/environment/git.nix
+++ modules/environment/git.nix
@@ -20,14 +20,12 @@
           GIT_AUTHOR_EMAIL = "philip.soitu@gmail.com";
           GIT_COMMITTER_NAME = GIT_AUTHOR_NAME;
           GIT_COMMITTER_EMAIL = GIT_AUTHOR_EMAIL;
+          GIT_CONFIG_COUNT = "2";
+          GIT_CONFIG_KEY_0 = "init.defaultBranch";
+          GIT_CONFIG_VALUE_0 = "main";
+          GIT_CONFIG_KEY_1 = "core.editor";
+          GIT_CONFIG_VALUE_1 = "nvim";
         };
-
-        args = [
-          "-c"
-          "init.defaultBranch=main"
-          "-c"
-          "core.editor=nvim"
-        ];
 
         runtimeInputs = [
           pkgs.neovim
