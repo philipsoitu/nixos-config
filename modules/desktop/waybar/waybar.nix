fcbc4caaa6c4a2060f7643218f0358191e5aaba5
philipsoitu <philip.soitu@gmail.com>
2026-05-03 15:07:42 -0400

    waybar stuff

---
+++ modules/desktop/waybar/waybar.nix
@@ -0,0 +1,34 @@
+{ self, inputs, ... }:
+{
+  flake.nixosModules.waybar =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = [
+        self.packages.${pkgs.stdenv.hostPlatform.system}.waybar
+      ];
+    };
+
+  perSystem =
+    { pkgs, ... }:
+    let
+      waybarConfig = pkgs.writeText "waybar-config.jsonc" (builtins.readFile ./waybar-config.jsonc);
+      waybarStyle = pkgs.writeText "waybar-style.css" (builtins.readFile ./waybar-style.css);
+    in
+    {
+      packages.waybar = inputs.wrappers.lib.wrapPackage {
+        inherit pkgs;
+        package = pkgs.waybar;
+
+        args = [
+          "--config"
+          "${waybarConfig}"
+          "--style"
+          "${waybarStyle}"
+        ];
+
+        runtimeInputs = with pkgs; [
+          pavucontrol
+        ];
+      };
+    };
+}
