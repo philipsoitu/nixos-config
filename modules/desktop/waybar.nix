c9c96511635a7e04ed85590e1a0b453e8ac48750
philipsoitu <philip.soitu@gmail.com>
2026-05-03 15:05:25 -0400

    fixed waybar

--- modules/desktop/waybar.nix
+++ modules/desktop/waybar.nix
@@ -13,77 +13,74 @@
     let
       waybarConfig = pkgs.writeText "waybar-config.jsonc" ''
         {
-          "mainBar": {
-            "layer": "top",
-            "position": "top",
-            "spacing": 0,
-            "height": 0,
-            "margin-top": 4,
-            "margin-right": 4,
-            "margin-bottom": 0,
-            "margin-left": 4,
-            "modules-left": ["hyprland/workspaces"],
-            "modules-center": ["clock"],
-            "modules-right": [
-              "tray",
-              "cpu",
-              "memory",
-              "battery",
-              "network",
-              "pulseaudio"
-            ],
-            "hyprland/workspaces": {
-              "disable-scroll": true,
-              "all-outputs": true,
-              "tooltip": false
-            },
-            "tray": {
-              "spacing": 10,
-              "tooltip": false
-            },
-            "clock": {
-              "format": "{:%I:%M %p - %a, %d %b %Y}",
-              "tooltip": false
-            },
-            "cpu": {
-              "format": "cpu {usage}%",
-              "interval": 2,
-              "states": {
-                "critical": 90
-              }
-            },
-            "memory": {
-              "format": "mem {percentage}%",
-              "interval": 2,
-              "states": {
-                "critical": 80
-              }
-            },
-            "battery": {
-              "format": "bat {capacity}%",
-              "interval": 5,
-              "states": {
-                "warning": 20,
-                "critical": 10
-              },
-              "tooltip": false
-            },
-            "network": {
-              "format-wifi": "conn.",
-              "format-ethernet": "conn.",
-              "format-disconnected": "disc.",
-              "interval": 5,
-              "tooltip": false
-            },
-            "pulseaudio": {
-              "scroll-step": 5,
-              "max-volume": 100,
-              "format": "vol {volume}%",
-              "format-bluetooth": "vol {volume}%",
-              "nospacing": 1,
-              "on-click": "pavucontrol",
-              "tooltip": false
+          "layer": "top",
+          "position": "top",
+          "spacing": 0,
+          "margin-top": 4,
+          "margin-right": 4,
+          "margin-bottom": 0,
+          "margin-left": 4,
+          "modules-left": ["hyprland/workspaces"],
+          "modules-center": ["clock"],
+          "modules-right": [
+            "tray",
+            "cpu",
+            "memory",
+            "battery",
+            "network",
+            "pulseaudio"
+          ],
+          "hyprland/workspaces": {
+            "disable-scroll": true,
+            "all-outputs": true,
+            "tooltip": false
+          },
+          "tray": {
+            "spacing": 10,
+            "tooltip": false
+          },
+          "clock": {
+            "format": "{:%I:%M %p - %a, %d %b %Y}",
+            "tooltip": false
+          },
+          "cpu": {
+            "format": "cpu {usage}%",
+            "interval": 2,
+            "states": {
+              "critical": 90
             }
+          },
+          "memory": {
+            "format": "mem {percentage}%",
+            "interval": 2,
+            "states": {
+              "critical": 80
+            }
+          },
+          "battery": {
+            "format": "bat {capacity}%",
+            "interval": 5,
+            "states": {
+              "warning": 20,
+              "critical": 10
+            },
+            "tooltip": false
+          },
+          "network": {
+            "format-wifi": "conn.",
+            "format-ethernet": "conn.",
+            "format-disconnected": "disc.",
+            "interval": 5,
+            "tooltip": false
+          },
+          "pulseaudio": {
+            "scroll-step": 5,
+            "max-volume": 100,
+            "format": "vol {volume}%",
+            "format-bluetooth": "vol {volume}%",
+            "nospacing": 1,
+            "on-click": "pavucontrol",
+            "tooltip": false
           }
         }
       '';
@@ -92,11 +89,9 @@
         * {
             border: none;
             border-radius: 0;
-            min-height: 0;
             font-family: "JetBrainsMono Nerd Font", monospace;
-            font-size: 12px;
+            font-size: 11px;
             font-weight: 500;
-            padding: 0;
         }
 
         window#waybar {
@@ -116,21 +111,24 @@
         #battery,
         #network,
         #pulseaudio {
-            margin: 6px 6px 6px 0;
-            padding: 2px 8px;
+            min-height: 18px;
+            margin: 4px 4px 4px 0;
+            padding: 1px 6px;
         }
 
         #workspaces {
+            min-height: 18px;
             background-color: #303536;
-            margin: 6px 0 6px 6px;
+            margin: 4px 0 4px 4px;
             border: 2px solid #434a4c;
         }
 
         #workspaces button {
             all: initial;
             min-width: 0;
-            box-shadow: inset 0 -3px transparent;
-            padding: 2px 4px;
+            min-height: 16px;
+            box-shadow: inset 0 -2px transparent;
+            padding: 1px 3px;
             color: #c7ab7a;
         }
 
