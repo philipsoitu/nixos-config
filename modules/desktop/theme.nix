ee7614f51ac704b4b1da50fd976fdb77b8dbdf5f
philipsoitu <philip.soitu@gmail.com>
2026-05-06 20:47:08 -0400

    theme colors fixed

--- modules/desktop/theme.nix
+++ modules/desktop/theme.nix
@@ -1,21 +1,21 @@
 let
   rose-pine-theme = {
-    base00 = "#232136"; # bg
-    base01 = "#2a273f"; # dark
+    base00 = "#232136";
+    base01 = "#2a273f";
     base02 = "#393552";
     base03 = "#6e6a86";
     base04 = "#908caa";
     base05 = "#e0def4";
-    base06 = "#9ccfd8"; # fg
-    base07 = "#c4a7e7"; # light fg
-    base08 = "#eb6f92"; # red
-    base09 = "#ea9a97"; # orange
-    base0A = "#f6c177"; # yellow
-    base0B = "#3e8fb0"; # green
-    base0C = "#9ccfd8"; # cyan
-    base0D = "#c4a7e7"; # blue
-    base0E = "#c4a7e7"; # magenta
-    base0F = "#56526e"; # orange
+    base06 = "#eb6f92";
+    base07 = "#f6c177";
+    base08 = "#ea9a97";
+    base09 = "#3e8fb0";
+    base0A = "#9ccfd8";
+    base0B = "#c4a7e7";
+    base0C = "#95b1ac";
+    base0D = "#2a283e";
+    base0E = "#44415a";
+    base0F = "#56526e";
   };
 
   stripHash =
