fcce70c78d8c2fe0ba262033148a4b22f1961240
philipsoitu <philip.soitu@gmail.com>
2026-05-08 00:02:35 -0400

    env added and new hostname

--- hosts/shellbox/configuration.nix
+++ hosts/shellbox/configuration.nix
@@ -8,14 +8,14 @@
     {
       imports = [
         self.nixosModules.shellboxHardware
+        self.nixosModules.environment
       ];
 
       # Bootloader.
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
 
-      networking.hostName = "nixos"; # Define your hostname.
-      # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+      networking.hostName = "shellbox";
 
       # Configure network proxy if necessary
       # networking.proxy.default = "http://user:password@proxy:port/";
