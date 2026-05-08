1a481c9e3d2523060cd7344dbee78e7e7bd681ed
philipsoitu <philip.soitu@gmail.com>
2026-05-08 00:03:23 -0400

    cleanup

--- hosts/shellbox/configuration.nix
+++ hosts/shellbox/configuration.nix
@@ -58,9 +58,6 @@
       # List packages installed in system profile. To search, run:
       # $ nix search wget
       environment.systemPackages = with pkgs; [
-        neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-        git
-        wget
       ];
 
       # Some programs need SUID wrappers, can be configured further or are
