6de876edc46361fb9a65d6fef00e4a13ead78f3d
philipsoitu <philip.soitu@gmail.com>
2026-05-13 16:04:47 -0400

    keybinds for window stuff

--- modules/environment/neovim/lua/keymaps.lua
+++ modules/environment/neovim/lua/keymaps.lua
@@ -3,3 +3,11 @@ vim.keymap.set({ 'n', 'i' }, '<C-s>', '<Esc>:w<CR>', { noremap = true, silent = 
 
 -- Buffer delete
 vim.keymap.set("n", "<leader>bd", "<cmd>bd<cr>")
+
+-- window
+vim.keymap.set('n', '<leader>wh', '<C-w>h', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>wj', '<C-w>j', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>wk', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>wl', '<C-w>l', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>wd', '<C-w>d', { noremap = true, silent = true })
