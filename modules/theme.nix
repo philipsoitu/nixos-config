7b0ef57f7850dc98ba9cffa2071a35d540163d1b
philipsoitu <philip.soitu@gmail.com>
2026-06-30 15:35:28 -0400

    cleanup

--- modules/theme.nix
+++ modules/theme.nix
@@ -13,17 +13,9 @@ let
     waybarStyle = ./desktop/waybar/gruvbox/style.css;
   };
 
-  stripHash =
-    str:
-    if builtins.substring 0 1 str == "#" then
-      builtins.substring 1 (builtins.stringLength str - 1) str
-    else
-      str;
-
-  themeNoHash = builtins.mapAttrs (_: v: stripHash v) theme;
 in
 {
   flake = {
-    inherit theme themeNoHash;
+    inherit theme;
   };
 }
